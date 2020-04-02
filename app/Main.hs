fecca79adba872d91a52f175f34c50ba514e5063
Vizaxo <vizaxo@mailfence.com>
2020-04-02 03:50:58 +0100

    Don't require the user to specify the shaders directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,6 @@
 module Main where
 
-import System.Environment
-
 import PathTracer.Loop
 
 main :: IO ()
-main = getArgs >>= \case
-  [path] -> pathTracer path
-  _ -> do
-    name <- getProgName
-    putStrLn ("Usage: " ++ name ++ " path_to_shader_directory")
+main = pathTracer "shaders"
